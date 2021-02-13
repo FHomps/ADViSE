8d2aa33bcfbee44278110a4001f510fd13733755
FHomps <hompsfrancois@gmail.com>
2021-02-13 02:02:02 +0000

    Fixed weights size for postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy import ndimage
 import torch
+from skimage.filters import rank
 
 def meanFilter(array, kernelSize):
-    return ndimage.convolve(array, np.full((kernelSize, kernelSize), 1.0 / (kernelSize ** 2)))
+    return ndimage.convolve(array, np.full((1, 1, kernelSize, kernelSize), 1.0 / (kernelSize ** 2)))
 
 # Transforms an image of the slope into a map of where the rover can reasonably go.
 # The slope threshold is in degrees.
@@ -11,6 +12,5 @@ def getViabilityMap(img, slopeThreshold=25, KSize_mean=5, KSize_median=5):
     img_np = img.detach().numpy()
     img_np = meanFilter(img_np, KSize_mean)
     img_np = np.where(img_np > slopeThreshold / 45, True, False)
-    img_np = ndimage.median_filter(img_np, size=KSize_median)
-    print(type(img_np))
+    img_np = ndimage.median_filter(img_np, size=(1, 1, KSize_median, KSize_median))
     return img_np
