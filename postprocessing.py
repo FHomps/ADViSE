7791026e46288de880f433419f3701b09e08c1bf
FHomps <hompsfrancois@gmail.com>
2021-02-13 04:30:51 +0000

    Improved filters

--- postprocessing.py
+++ postprocessing.py
@@ -1,16 +1,13 @@
 import numpy as np
 from scipy import ndimage
 import torch
-from skimage.filters import rank
 
-def meanFilter(array, kernelSize):
-    return ndimage.convolve(array, np.full((1, 1, kernelSize, kernelSize), 1.0 / (kernelSize ** 2)))
 
 # Transforms an image of the slope into a map of where the rover can reasonably go.
 # The slope threshold is in degrees.
-def getViabilityMap(img, slopeThreshold=25, KSize_mean=5, KSize_median=5):
+def getViabilityMap(img, slopeThreshold=25, K_mean=5, percent_spread=50, K_spread=5):
     img_np = img.detach().numpy()
-    img_np = meanFilter(img_np, KSize_mean)
+    img_np = ndimage.uniform_filter(img_np, size=(1, 1, K_mean, K_mean))
     img_np = np.where(img_np > slopeThreshold / 45, True, False)
-    img_np = ndimage.median_filter(img_np, size=(1, 1, KSize_median, KSize_median))
+    img_np = np.logical_or(ndimage.percentile_filter(img_np, percent_spread, size=(1, 1, K_spread, K_spread)), img_np)
     return img_np
